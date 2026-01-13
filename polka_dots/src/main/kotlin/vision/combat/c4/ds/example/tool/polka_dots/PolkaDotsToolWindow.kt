b98d5003f1d67feb06121e431ad9dd6a1244547b
Artem Odarchuk <a.m.odarchuk@gmail.com>
2026-01-13 16:35:22 +0200

    change ui, start adding mass processing

--- polka_dots/src/main/kotlin/vision/combat/c4/ds/example/tool/polka_dots/PolkaDotsToolWindow.kt
+++ polka_dots/src/main/kotlin/vision/combat/c4/ds/example/tool/polka_dots/PolkaDotsToolWindow.kt
@@ -1,28 +1,28 @@
 package vision.combat.c4.ds.example.tool.polka_dots
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.flow.Flow
 import vision.combat.c4.ds.sdk.ui.component.ColorSelector
 import vision.combat.c4.ds.sdk.ui.component.WindowScaffold
 import vision.combat.c4.ds.sdk.ui.component.bar.BackNavTopAppBar
-import vision.combat.c4.ds.sdk.ui.component.button.Button
 import vision.combat.c4.ds.sdk.ui.component.checkable.SwitchField
 import vision.combat.c4.ds.sdk.ui.component.text.OutlinedTextInputField
-import vision.combat.c4.ds.sdk.ui.util.showToast
 import vision.combat.c4.ds.sdk.ui.viewmodel.diViewModel
 import vision.combat.c4.ds.tool.sample.window.R
 
@@ -37,43 +37,50 @@ internal fun PolkaDotsToolWindow(viewModel: PolkaDotsToolViewModel = diViewModel
 @Composable
 private fun WindowContent(uiState: PolkaDotsToolViewModel.UiState, viewModel: PolkaDotsToolViewModel) {
     WindowScaffold(
-        topAppBar = { TopAppBar() },
+        topAppBar = { PolkaTopAppBar(viewModel) },
     ) {
-        OutlinedTextInputField(
-            value = uiState.distance,
-            onValueChange = { viewModel.updateDistance(it) },
-            label = stringResource(R.string.step_in_meters),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
-        )
-        Row(modifier = Modifier.fillMaxWidth()) {
+        Row(modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)) {
+            OutlinedTextInputField(
+                modifier = Modifier.weight(1f),
+                value = uiState.distance,
+                onValueChange = { viewModel.updateDistance(it) },
+                label = stringResource(R.string.step_in_meters),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            if (!uiState.isPrefixFromModel) {
                 OutlinedTextInputField(
                     modifier = Modifier.weight(1f),
                     value = uiState.prefix,
                     onValueChange = { viewModel.updatePrefix(it) },
                     label = stringResource(R.string.prefix),
                 )
-                OutlinedTextInputField(
-                    modifier = Modifier.weight(1f),
-                    value = uiState.startingNumber,
-                    onValueChange = { viewModel.updateStartingNumber(it) },
-                    label = stringResource(R.string.starting_number),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-                )
+                Spacer(modifier = Modifier.width(4.dp))
+            }
+            OutlinedTextInputField(
+                modifier = Modifier.weight(1f),
+                value = uiState.startingNumber,
+                onValueChange = { viewModel.updateStartingNumber(it) },
+                label = stringResource(R.string.starting_number),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
         }
-        Row (
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                stringResource(R.string.choose_points_color),
-                modifier = Modifier.weight(1f)
-            )
-            ColorSelector(
-                uiState.color,
-                onSelectColorClick = { viewModel.updateColor(it) },
-                modifier = Modifier.padding(end = 10.dp),
-                readOnly = false
-            )
+        if (!uiState.isColorFromModel) {
+            Row (
+                modifier = Modifier.fillMaxWidth().padding(top = 16.dp, start = 16.dp, end = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    stringResource(R.string.choose_points_color),
+                    modifier = Modifier.weight(1f)
+                )
+                ColorSelector(
+                    uiState.color,
+                    onSelectColorClick = { viewModel.updateColor(it) },
+                    modifier = Modifier.padding(end = 10.dp),
+                    readOnly = false
+                )
+            }
         }
         SwitchField(
             initialValue = uiState.isColorFromModel,
@@ -86,8 +93,8 @@ private fun WindowContent(uiState: PolkaDotsToolViewModel.UiState, viewModel: Po
             onCheckedChange = { viewModel.updateIsPrefixFromModel(it) }
         )
         SwitchField(
-            initialValue = uiState.isModelRetained,
-            stringResource(R.string.is_model_retained),
+            initialValue = uiState.isModelDeleted,
+            stringResource(R.string.is_model_deleted),
             onCheckedChange = { viewModel.updateIsModelRetained(it) }
         )
         SwitchField(
@@ -95,18 +102,24 @@ private fun WindowContent(uiState: PolkaDotsToolViewModel.UiState, viewModel: Po
             stringResource(R.string.is_reversed_order),
             onCheckedChange = { viewModel.updateIsReversedOrder(it) }
         )
-        Button(
-            label = stringResource(R.string.process_model),
-            onClick = { viewModel.process() },
-            enabled = viewModel.uiState.selectedModel != null,
-            modifier = Modifier.align(alignment = Alignment.CenterHorizontally)
-        )
     }
 }
 
 @Composable
-private fun TopAppBar() {
+private fun PolkaTopAppBar(viewModel: PolkaDotsToolViewModel) {
     BackNavTopAppBar(
         title = stringResource(R.string.polka_dots_tool_name),
+        actions = {
+            IconButton(
+                onClick = { viewModel.process() },
+                enabled = viewModel.uiState.selectedModel != null,
+                modifier = Modifier.padding(horizontal = 8.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Check,
+                    contentDescription = stringResource(R.string.select_the_model),
+                )
+            }
+        },
     )
 }
