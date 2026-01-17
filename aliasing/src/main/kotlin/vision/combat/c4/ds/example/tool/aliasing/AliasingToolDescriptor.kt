5d42db2886eab203e388414be7305f222b5f8107
Artem Odarchuk <a.m.odarchuk@gmail.com>
2026-01-17 12:12:59 +0200

    init aliasing plugin

---
+++ aliasing/src/main/kotlin/vision/combat/c4/ds/example/tool/aliasing/AliasingToolDescriptor.kt
@@ -0,0 +1,24 @@
+package vision.combat.c4.ds.example.tool.aliasing
+
+import org.kodein.di.DI
+import vision.combat.c4.ds.sdk.tool.AbstractTool
+import vision.combat.c4.ds.sdk.tool.ToolContext
+import vision.combat.c4.ds.sdk.tool.ToolDescriptor
+import vision.combat.c4.ds.sdk.tool.ToolId
+import vision.combat.c4.ds.sdk.tool.ToolParams
+import vision.combat.c4.ds.tool.sample.aliasing.R
+
+class AliasingToolDescriptor(toolContext: ToolContext) : ToolDescriptor(toolContext) {
+
+    override val id: ToolId = ID
+    override val iconResId: Int = R.drawable.ic_tool
+    override val nameResId: Int = R.string.aliasing_tool_name
+
+    override fun createTool(toolContext: ToolContext, di: DI, params: ToolParams?): AbstractTool {
+        return AliasingTool(toolContext, this, di, params)
+    }
+
+    companion object {
+        val ID = ToolId<AliasingToolDescriptor>()
+    }
+}
---
+++ aliasing/src/main/kotlin/vision/combat/c4/ds/example/tool/aliasing/AliasingToolWindow.kt
@@ -0,0 +1,105 @@
+package vision.combat.c4.ds.example.tool.aliasing
+
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.PlaylistAddCheck
+import androidx.compose.material.icons.filled.PublishedWithChanges
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.flow.Flow
+import vision.combat.c4.ds.sdk.ui.component.WindowScaffold
+import vision.combat.c4.ds.sdk.ui.component.bar.BackNavTopAppBar
+import vision.combat.c4.ds.sdk.ui.component.checkable.SwitchField
+import vision.combat.c4.ds.sdk.ui.component.text.OutlinedTextInputField
+import vision.combat.c4.ds.sdk.ui.util.showToast
+import vision.combat.c4.ds.sdk.ui.viewmodel.diViewModel
+import vision.combat.c4.ds.tool.sample.aliasing.R
+
+@Composable
+internal fun AliasingToolWindow(viewModel: AliasingToolViewModel = diViewModel()) {
+    WindowContent(
+        uiState = viewModel.uiState,
+        viewModel = viewModel
+    )
+    EffectHandler(viewModel.effects)
+
+    val context = LocalContext.current
+
+    val assets = context
+        .resources
+        .getStringArray(R.array.alias_assets_list)
+        .toList()
+    viewModel.initAssets(assets)
+}
+
+@Composable
+private fun WindowContent(uiState: AliasingToolViewModel.UiState, viewModel: AliasingToolViewModel) {
+    WindowScaffold(
+        topAppBar = { AliasingTopAppBar(viewModel) },
+    ) {
+        SwitchField(
+            initialValue = uiState.isRandomized,
+            stringResource(R.string.is_randomized),
+            onCheckedChange = { viewModel.updateIsRandomized(it) }
+        )
+        if (!uiState.isRandomized) {
+            OutlinedTextInputField(
+                value = uiState.aliasList,
+                label = stringResource(R.string.alias_list_text),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .defaultMinSize(minHeight = 260.dp),
+                onValueChange = { viewModel.updateAliasList(it) },
+                singleLine = false,
+            )
+        }
+    }
+}
+
+@Composable
+private fun AliasingTopAppBar(viewModel: AliasingToolViewModel) {
+    BackNavTopAppBar(
+        title = stringResource(R.string.aliasing_tool_name),
+        actions = {
+            IconButton(
+                onClick = {
+                    viewModel.massProcess()
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.PlaylistAddCheck,
+                    contentDescription = stringResource(R.string.select_the_model),
+                )
+            }
+        }
+    )
+}
+
+@Composable
+private fun EffectHandler(effectFlow: Flow<Effect>) {
+    val context = LocalContext.current
+
+    LaunchedEffect(effectFlow, context) {
+        effectFlow.collect { effect ->
+            val message = when (effect) {
+                Effect.ModelNotSelected -> R.string.no_model_selected
+                Effect.NoLayers -> R.string.no_layers
+                Effect.NoAliases -> R.string.no_aliases
+                Effect.NotEnoughAliases -> R.string.not_enough_aliases
+                Effect.Done -> R.string.done
+            }
+            context.showToast(message)
+        }
+    }
+}
+
