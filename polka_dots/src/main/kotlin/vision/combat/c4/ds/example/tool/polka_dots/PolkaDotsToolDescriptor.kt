16b4bc5bdad7f0a0cac6b40274a59377e0d9320b
Artem Odarchuk <a.m.odarchuk@gmail.com>
2026-01-19 20:23:19 +0200

    bodge the stringResource bug

--- polka_dots/src/main/kotlin/vision/combat/c4/ds/example/tool/polka_dots/PolkaDotsToolDescriptor.kt
+++ polka_dots/src/main/kotlin/vision/combat/c4/ds/example/tool/polka_dots/PolkaDotsToolDescriptor.kt
@@ -12,7 +12,7 @@ class PolkaDotsToolDescriptor(toolContext: ToolContext) : ToolDescriptor(toolCon
 
     override val id: ToolId = ID
     override val iconResId: Int = R.drawable.baseline_grain_24
-    override val nameResId: Int = R.string.polka_dots_tool_name
+    override val nameResId: Int = R.string.polka_dots_plugin_tool_name
 
     override fun createTool(toolContext: ToolContext, di: DI, params: ToolParams?): AbstractTool {
         return PolkaDotsTool(toolContext, this, di, params)
